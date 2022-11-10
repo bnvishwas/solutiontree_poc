328b566b649a8d22ba29ea3e916e4cb4eaa1c233
vishwasbn <53083705+vishwasbn@users.noreply.github.com>
2022-11-10 10:39:45 +0530

    - derived the slot data from users working hour

--- force-app/main/default/lwc/cpqcomponent/cpqcomponent.js
+++ force-app/main/default/lwc/cpqcomponent/cpqcomponent.js
@@ -1,26 +1,36 @@
 import { LightningElement, api, track, wire } from 'lwc';
-import getcontactrecords from '@salesforce/apex/ContactRecordsController.getcontactrecords1';
+//import getcontactrecords from '@salesforce/apex/ContactRecordsController.getcontactrecords1';
 //import getcontactrecor from '@salesforce/apex/ContactRecordsController.getEventdata';
+import getSpeakerslotdata from '@salesforce/apex/SpeakerListController.getSpeakerRecords';
 import { getPicklistValues } from 'lightning/uiObjectInfoApi';
 import Country_Region from '@salesforce/schema/Account.Country_Region__c';
 import rating_Value from '@salesforce/schema/Contact.Ratings__c';
 
+/*const cols = [
+    { label: 'First Name', fieldName: 'FirstName' },
+    { label: 'Last Name', fieldName: 'LastName' },
+    { label: 'Email', fieldName: 'Email' },
+    { label: 'Subject', fieldName: 'Subject' },
+    { label: 'StartDateTime', fieldName: 'StartDateTime' },
+    { label: 'EndDateTime', fieldName: 'EndDateTime' }
+]*/
+
 const cols = [
-    {label : 'First Name', fieldName : 'FirstName'},
-    {label: 'Last Name', fieldName: 'LastName'},
-    {label: 'Email', fieldName: 'Email'},
-    {label:'Subject', fieldName: 'Subject'},
-    {label:'StartDateTime', fieldName: 'StartDateTime'},
-    {label:'EndDateTime', fieldName: 'EndDateTime'}
+    { label: 'Name', fieldName: 'Name' },
+    { label: 'Rating', fieldName: 'Rating' },
+    { label: 'Email', fieldName: 'Email' },
+    { label: 'Region', fieldName: 'Region' },
+    { label: 'start_time', fieldName: 'start_time' },
+    { label: 'end_time', fieldName: 'end_time' }
 ]
 
 export default class Cpqcomponent extends LightningElement {
-connectedCallback(){
-    this.loadspeaker();
-}
+    connectedCallback() {
+        this.loadspeaker();
+    }
     @api contactRecords;
     @api columns = cols;
-@track Eventdate;
+    @track Eventdate;
     @track regionvalue;
     @track countryregionlist;
     @wire(getPicklistValues, { recordTypeId: '012000000000000AAA', fieldApiName: Country_Region })
@@ -55,31 +65,31 @@ connectedCallback(){
     //     console.log('error : '+error.message);
     // })
     // }
-   /* @wire (getcontactrecords)
-    getcontactrecords({data, error})
-    {
-        if(data)
-        {
-            this.contactRecords = data;
-        }
-        else if(error)
-        {
-            console.log('Error Message has been Occured.'+ error);
-        }
-    } */
+    /* @wire (getcontactrecords)
+     getcontactrecords({data, error})
+     {
+         if(data)
+         {
+             this.contactRecords = data;
+         }
+         else if(error)
+         {
+             console.log('Error Message has been Occured.'+ error);
+         }
+     } */
 
-    handleregionChange(event){
-        this.regionvalue=event.target.value;
+    handleregionChange(event) {
+        this.regionvalue = event.target.value;
         this.loadspeaker();
     }
 
-    handleratingChange(event){
-        this.ratingvalue=event.target.value;
+    handleratingChange(event) {
+        this.ratingvalue = event.target.value;
         this.loadspeaker();
     }
 
-    Eventdatehandle(event){
-        this.Eventdate=event.target.value;
+    Eventdatehandle(event) {
+        this.Eventdate = event.target.value;
         this.loadspeaker();
     }
 
@@ -101,7 +111,7 @@ connectedCallback(){
     //     .catch((error)=>{
     //         console.log('errora : '+error.message);
     //     })
-        
+
     // }
 
     loadspeaker() {
@@ -109,33 +119,80 @@ connectedCallback(){
         var region = this.regionvalue;
         var d = this.Eventdate;
         if (this.regionvalue == undefined) {
-          region = null;
+            region = null;
         }
         if (this.ratingvalue == undefined) {
-          rating = null;
+            rating = null;
         }
         if (this.Eventdate == undefined) {
             d = null;
-          }
-        getcontactrecords({ region: region, rating: rating, d: d })
-          .then((data) => {
-            var resultdata = JSON.parse(JSON.stringify(data));
-            var modifieddata = [];
-            for(var item in resultdata){
-    
-              var speakerdata = resultdata[item];
-              speakerdata['FirstName'] = speakerdata.Who.FirstName;
-              speakerdata['LastName'] = speakerdata.Who.LastName;
-              speakerdata['Email'] = speakerdata.Who.Email;
-              speakerdata['speakerId'] = speakerdata.Who.Id;
-              modifieddata.push(speakerdata);
-    
+        }
+        //apex caontroller - which derives the slot 
+
+
+        getSpeakerslotdata({region : region, rating : rating})
+        .then((response)=>{
+            console.log('Data from Apex ');
+            console.log(JSON.stringify(response));
+
+            var resposedata = JSON.parse(response);
+            var modifiedspeakerlist = []; 
+            var index = 0;
+            for(var item in resposedata){
+                var speakerdata = Object.assign({},resposedata[item]);
+                for(var entry in speakerdata.Available_Slot){
+                    var timeslot = speakerdata.Available_Slot[entry];
+                    var slotdetails = [];
+                    slotdetails = Object.assign({},speakerdata);
+                    slotdetails['start_time'] = timeslot.start_time;
+                    slotdetails['end_time'] = timeslot.end_time;
+                    slotdetails['index'] = index;
+                    modifiedspeakerlist.push(slotdetails);
+                    index++;
+                }
             }
-            this.speakerlist = modifieddata;
-          })
-          .catch((error) => {
-            console.log('errora : ' + error.message);
-          })
-    
+            this.speakerlist = modifiedspeakerlist;
+            JSON.stringify('modified list '+this.speakerlist);
+        })
+        .catch((error)=>{
+            this.speakerlist = [];
+            console.log('Error on apex call : '+error.message);
+        })
+
+        /*
+        getcontactrecords({ region: region, rating: rating, d: d })
+            .then((data) => {
+                var resultdata = JSON.parse(JSON.stringify(data));
+                var modifieddata = [];
+                for (var item in resultdata) {
+
+                    var speakerdata = resultdata[item];
+                    speakerdata['FirstName'] = speakerdata.Who.FirstName;
+                    speakerdata['LastName'] = speakerdata.Who.LastName;
+                    speakerdata['Email'] = speakerdata.Who.Email;
+                    speakerdata['speakerId'] = speakerdata.Who.Id;
+                    modifieddata.push(speakerdata);
+
+                }
+                this.speakerlist = modifieddata;
+            })
+            .catch((error) => {
+                console.log('errora : ' + error.message);
+            })
+        */ //event data from APex controller
+    }
+
+    getmoddeddate(time) {
+        var formatteddate = undefined;
+        if (time != undefined) {
+          var jsdate = new Date(time);
+          var ampm = hours >= 12 ? "PM" : "AM";
+          hours = hours % 12;
+          hours = hours ? hours : 12; // the hour '0' should be '12'
+          minutes = minutes < 10 ? "0" + minutes : minutes;
+          var strTime = hours + ":" + minutes + " " + ampm;
+          return strTime;
+        }
+        return formatteddate;
       }
 }
